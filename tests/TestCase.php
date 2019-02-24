a29f549b072fdb7823f62f9a7a6aac1bf12948fe
Crapo <vincent@girol.fr>
2019-02-24 02:15:06 +0100

    Update composer autoload

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,21 +1,9 @@
 <?php
 
-namespace VGirol\JsonApiAssert\Laravel\Tests;
+namespace VGirol\JsonApiAssert\Tests;
 
-use Orchestra\Testbench\TestCase as BaseTestCase;
-use VGirol\JsonApiAssert\Laravel\JsonApiAssertServiceProvider;
+use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    protected $mediaType = 'application/vnd.api+json';
-
-    /**
-     * Load package service provider
-     * @param  \Illuminate\Foundation\Application $app
-     * @return VGirol\JsonApiAssert\Laravel\JsonApiAssertServiceProvider
-     */
-    protected function getPackageProviders($app)
-    {
-        return [JsonApiAssertServiceProvider::class];
-    }
 }
